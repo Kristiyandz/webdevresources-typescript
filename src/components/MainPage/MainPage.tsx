13286b9d43e0dbd6e314435705fba396e4a0e1fe
Kristiyandz <kristiyandzhakov@gmail.com>
2019-10-17 05:52:46 +0100

    Initial commit

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC, ReactElement } from 'react';
+import styles from './MainPage.module.scss';
+
+const MainPage: FC = (): ReactElement => {
+  return (
+    <div className={styles.MainWrapper}>
+
+    </div>
+  );
+};
+
+export default MainPage;
